a41cf9db27cb3bcc4665aa44630f8fae5f3a63fe
Tobias Rosa <tobias.rosa2000@gmail.com>
2022-09-11 15:58:56 -0300

    Setup prismicio

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
 import '../styles/globals.css'
 
+import {PrismicPreview} from '@prismicio/next'
+import {PrismicProvider} from '@prismicio/react'
 import type {AppProps} from 'next/app'
+import Link from 'next/link'
+
+import {linkResolver, repositoryName} from '/prismicio'
 
 function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({href, ...props}) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
 
 export default MyApp
